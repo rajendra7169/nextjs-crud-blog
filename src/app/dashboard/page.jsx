04d82902aebce1ab61cd77d49df79aa778adaffa
Rajendra Pandey <30778680+rajendra7169@users.noreply.github.com>
2024-09-03 18:03:49 +0545

    Update page.jsx

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -87,7 +87,7 @@ const Dashboard = () => {
           <h1>Add New Post (every field required)</h1>
           <input type="text" placeholder="Title" className={styles.input} />
           <input type="text" placeholder="Desc" className={styles.input} />
-          <input type="text" placeholder="Image" className={styles.input} />
+          <input type="text" placeholder="Image Link" className={styles.input} />
           <textarea
             placeholder="Content"
             className={styles.textArea}
